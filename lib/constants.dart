b1832f71f8d2652e8f1e89a5ba36eb0368351b6f
KeysGilbert <gbk5070@psu.edu>
2022-01-06 20:41:28 -0500

    moved constants to one file

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const TextStyle labelTextStyle =
+    TextStyle(fontSize: 18, color: Color(0xFF8D8E98));
+const double bottomHeight = 80.0;
+
+const Color inactiveCardColor = Color(0xFF111328);
+const Color tappedCardColor = Color(0xFF1d1e33);
+const TextStyle numberStyle =
+    TextStyle(fontWeight: FontWeight.w900, fontSize: 50);
