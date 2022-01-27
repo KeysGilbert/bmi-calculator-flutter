4251c829ca4ff78db3f8e7a0a55200450caaf988
KeysGilbert <gbk5070@psu.edu>
2022-01-27 13:26:10 -0500

    new constant

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,3 +8,5 @@ const Color inactiveCardColor = Color(0xFF111328);
 const Color tappedCardColor = Color(0xFF1d1e33);
 const TextStyle numberStyle =
     TextStyle(fontWeight: FontWeight.w900, fontSize: 50);
+const TextStyle largeButtonText =
+    TextStyle(fontSize: 25, fontWeight: FontWeight.bold);
