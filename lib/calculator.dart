025164f76eb19cad0c4b94fa6ff900d6f74ab0ec
KeysGilbert <gbk5070@psu.edu>
2022-01-27 15:43:32 -0500

    added functions to display weight  range and message

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -8,4 +8,28 @@ class Calculator {
 
     return bmi.round().toDouble();
   }
+
+  String bmiRange() {
+    if (calculateBMI(userWeight, userHeight) >= 30.0) {
+      return "Obese";
+    } else if (calculateBMI(userWeight, userHeight) >= 25 &&
+        calculateBMI(userWeight, userHeight) <= 29.9) {
+      return "Overweight";
+    } else if (calculateBMI(userWeight, userHeight) >= 18.5 &&
+        calculateBMI(userWeight, userHeight) <= 24.9) {
+      return "Normal";
+    } else {
+      return "Underweight";
+    }
+  }
+
+  String bmiMessage() {
+    if (bmiRange() == "Obese") {
+      return "It's time to diet and exercise";
+    } else if (bmiRange() == "Normal") {
+      return "You are doing just fine. Keep it up!";
+    } else {
+      return "Might need to eat some more";
+    }
+  }
 }
